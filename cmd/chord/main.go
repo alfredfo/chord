aa6e989550a525d05d87e787403af3ec0ab95de0
Darko Petrov <darko.petrov1099@gmail.com>
2023-12-06 11:38:26 +0100

    add call func

--- cmd/chord/main.go
+++ cmd/chord/main.go
@@ -85,6 +85,22 @@ func NewNode(id NodeAddress, addr *net.TCPAddr) (*Node, error) {
 	}, nil
 }
 
+func call(rpcname string, addr net.TCPAddr, args interface{}, reply interface{}) error {
+	c, err := rpc.DialHTTP("tcp", addr.String())
+	if err != nil {
+		log.Printf("error dialing: %v", err)
+		return err
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err != nil {
+		log.Printf("error calling: %v", err)
+		return err
+	}
+	return nil
+}
+
 const m = 160
 
 func main() {
