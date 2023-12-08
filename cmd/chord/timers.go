de86cd982132cf6dc3cacfbfcc7f74193b606e05
Alfred Persson Forsberg <cat@catcream.org>
2023-12-08 22:10:50 +0100

    implement stabilize
    Signed-off-by: Alfred Persson Forsberg <cat@catcream.org>

--- cmd/chord/timers.go
+++ cmd/chord/timers.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"time"
+
+	"github.com/alfredfo/chord/api"
+	"github.com/alfredfo/chord/hashing"
+	"github.com/alfredfo/chord/transport"
 )
 
-func stabilizeTimer(ms int) {
+func stabilizeTimer(node *api.Node, ms int) {
 	for !finished {
 		time.Sleep(time.Millisecond * time.Duration(ms))
+		node.Mu.Lock()
+		defer node.Mu.Unlock()
+
+		x := node.Successor // WRONG ask for pred ... send()
+
+		if hashing.SBetween(node.NodeInfo.ID, x.ID, node.Successor.ID, false) {
+			node.Successor = x
+		}
+		transport.SendNotify(node, node.Successor)
 	}
 }
 
-func checkPredecessorTimer(ms int) {
+func checkPredecessorTimer(node *api.Node, ms int) {
 	for !finished {
 		time.Sleep(time.Millisecond * time.Duration(ms))
+		node.Mu.Lock()
+		defer node.Mu.Unlock()
 	}
 }
 
-func fixFingersTimer(ms int) {
+func fixFingersTimer(node *api.Node, ms int) {
 	for !finished {
 		time.Sleep(time.Millisecond * time.Duration(ms))
+		node.Mu.Lock()
+		defer node.Mu.Unlock()
 	}
 }
