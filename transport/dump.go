25f053188a2cf413891e514ff341eada5699ac22
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-12-08 11:03:10 +0100

    Fix dump.

--- transport/dump.go
+++ transport/dump.go
@@ -1,7 +1,7 @@
 package transport
 
 import (
-  "log"
+  "fmt"
 	"net"
 
   // "github.com/alfredfo/chord/api"
@@ -10,20 +10,21 @@ import (
 type DumpRPCArgs struct {
 }
 type DumpRPCReply struct {
+  DumpString string
 }
 
 func (tp *TransportNode) Dump(args *DumpRPCArgs, reply *DumpRPCReply) error {
-  log.Printf("tcpAdd: %v, succ: %v, pred: %v, kv stored: %v\n", 
-    tp.Node.Address, tp.Node.Successor, tp.Node.Successor, tp.Node.Bucket)
+  reply.DumpString = fmt.Sprintf("tcpAdd: %v, succ: %v, pred: %v, kv stored: %v\n", 
+    tp.Node.Address, tp.Node.Successor, tp.Node.Predecessor, tp.Node.Bucket)
 	return nil
 }
 
-func SendDump(addr *net.TCPAddr) error {
+func SendDump(addr *net.TCPAddr) (string, error) {
 	args := DumpRPCArgs{}
 	reply := DumpRPCReply{}
 	err := call("TransportNode.Dump", addr, &args, &reply)
 
-	return err
+	return reply.DumpString, err
 }
 
 
