447fc508489ca09257a731006a8d3ede8a00ea76
digital-nomad-cheng <vincentcheng929@gmail.com>
2023-12-10 23:02:06 +0100

    Fix tls.

--- cmd/chord/serve.go
+++ cmd/chord/serve.go
@@ -3,72 +3,52 @@ package main
 import (
 	"github.com/alfredfo/chord/transport"
 	"log"
-	"net"
-	"net/http"
+  "crypto/tls"
+  "crypto/rand"
 	"net/rpc"
 )
 
-// func serve(tpn *transport.TransportNode) error {
-
-// 	if err := rpc.Register(tpn); err != nil {
-// 		log.Fatal("Failed to register RPC method")
-// 	}
-// 	cert, err := tls.LoadX509KeyPair("server.crt", "server.key")
-// 	if err != nil {
-// 		log.Fatalf("server: loadkeys: %s", err)
-// 	}
-// 	if len(cert.Certificate) != 2 {
-// 		log.Fatal("server.crt should have 2 concatenated certificates: server + CA")
-// 	}
-// 	ca, err := x509.ParseCertificate(cert.Certificate[1])
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	certPool := x509.NewCertPool()
-// 	certPool.AddCert(ca)
-// 	config := tls.Config{
-// 		Certificates: []tls.Certificate{cert},
-// 		ClientAuth:   tls.RequireAndVerifyClientCert,
-// 		ClientCAs:    certPool,
-// 	}
-// 	config.Rand = rand.Reader
-// 	node := tpn.Node
-// 	addr := node.NodeInfo.TCPAddr
-
-// 	service := addr.String()
-// 	listener, err := tls.Listen("tcp", service, &config)
-// 	if err != nil {
-// 		log.Fatalf("server: listen: %s", err)
-// 	}
-
-// 	rpc.Register(tpn)
-
-// 	log.Println("listening on: ", addr)
-// 	l, e := net.ListenTCP("tcp", &addr)
-// 	if e != nil {
-// 		log.Fatal("listen error:", e)
-// 	}
-// 	err := http.ServeTLS(l, nil, "cert.crt", "key.key")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	return nil
-// }
-
 func serve(tpn *transport.TransportNode) error {
-	rpc.Register(tpn)
-	rpc.HandleHTTP()
-
-	node := tpn.Node
-	addr := node.NodeInfo.TCPAddr
-	log.Println("listening on: ", addr)
-	l, e := net.ListenTCP("tcp", &addr)
-	if e != nil {
-		log.Fatal("listen error:", e)
+	if err := rpc.Register(tpn); err != nil {
+		log.Fatal("Failed to register RPC method")
 	}
-	err := http.ServeTLS(l, nil, "certs/signed.crt", "certs/cert.key")
+	cert, err := tls.LoadX509KeyPair("certs/signed.crt", "certs/cert.key")
 	if err != nil {
-		log.Fatalf("error in ServeTLS: %v\n", err)
+		log.Fatalf("server: loadkeys: %s", err)
 	}
-	return nil
+  config := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		Rand:         rand.Reader,
+	}
+  listener, err := tls.Listen("tcp", tp.Node.NodeInfo.TCPAddr.String(), config)
+  if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+  // log.Println("RPC server is running on port 1234...")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go rpc.ServeConn(conn)
+	}
 }
+
+//func serve(tpn *transport.TransportNode) error {
+//	rpc.Register(tpn)
+//	rpc.HandleHTTP()
+//
+//	node := tpn.Node
+//	addr := node.NodeInfo.TCPAddr
+//	log.Println("listening on: ", addr)
+//	l, e := net.ListenTCP("tcp", &addr)
+//	if e != nil {
+//		log.Fatal("listen error:", e)
+//	}
+//	err := http.ServeTLS(l, nil, "certs/signed.crt", "certs/cert.key")
+//	if err != nil {
+//		log.Fatalf("error in ServeTLS: %v\n", err)
+//	}
+//	return nil
+//}
--- transport/rpc.go
+++ transport/rpc.go
@@ -2,7 +2,6 @@ package transport
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"log"
 	"net"
 	"net/rpc"
@@ -13,31 +12,18 @@ func call(rpcName string, addr *net.TCPAddr, args interface{}, reply interface{}
 	if err != nil {
 		log.Fatalf("client: loadkeys: %s", err)
 	}
-	if len(cert.Certificate) != 2 {
-		log.Fatal("client.crt should have 2 concatenated certificates: client + CA")
+	config := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		InsecureSkipVerify: true, // For testing purposes only, consider removing this in production
 	}
-	ca, err := x509.ParseCertificate(cert.Certificate[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	certPool := x509.NewCertPool()
-	certPool.AddCert(ca)
-	// config := tls.Config{
-	// 	Certificates: []tls.Certificate{cert},
-	// 	RootCAs:      certPool,
-	// }
-	config := tls.Config{
-		RootCAs: certPool,
-	}
-	conn, err := tls.Dial("tcp", addr.String(), &config)
-
+  conn, err := tls.Dial("tcp", addr.String(), config)
 	if err != nil {
 		log.Fatalf("client: dial: %s", err)
 	}
 
 	defer conn.Close()
 
-	log.Println("client: connected to: ", conn.RemoteAddr())
+	// log.Println("client: connected to: ", conn.RemoteAddr())
 	rpcClient := rpc.NewClient(conn)
 
 	if err := rpcClient.Call(rpcName, args, reply); err != nil {
